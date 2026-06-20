8390e634d0094d81cbf2e28e9992e538aa0ed2f2
romario <rrr_rodrigues@hotmail.com>
2026-06-20 00:56:01 -0300

    feat: criação das telas de jogos e ranking

---
+++ Android/app/ranking.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Ranking() {
+  return (
+    <View style={{ padding: 20 }}>
+      <Text>Ranking Geral</Text>
+    </View>
+  );
+}
---
+++ Android/types/Match.ts
@@ -0,0 +1,8 @@
+export interface Match {
+  id: number;
+  teamHome: string;
+  teamAway: string;
+  date: string;
+  scoreHome?: number;
+  scoreAway?: number;
+}
